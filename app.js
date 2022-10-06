8b554bbb97e64ed3ad81b36955c1a0cdd3231676
shubhom <shbhomsrivastav@gmail.com>
2022-10-06 16:44:32 +0530

    weather app done

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
   let long;
   let lat;
-  let temperatureDescription= document.querySelector(".description");
-  
-  let temperatureDegree= document.querySelector(".temp-degree");
-  
-  let locationTimeZone= document.querySelector(".location-tz");
+  let temperatureDescription = document.querySelector(".description");
+  let temperatureDegree = document.querySelector(".temp-degree");
+  let locationTimeZone = document.querySelector(".location-tz");
 
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position=>{
-       lat = position.coords.latitude;
-       long = position.coords.longitude;
-
-      let key='55a4ca8740b184486c6fad1b157bc6a5';
-      let api='https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&units=metric&appid='+key;
-        fetch(api)
-          .then(response=>{
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      //api info
+      let key = '55a4ca8740b184486c6fad1b157bc6a5';
+      let api = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric&appid=' + key;
+      fetch(api)
+        .then(response => {
           return response.json();
         })
-        .then(data =>{
+        .then(data => {
           console.log(data);
-          const {temp}= data.main;
-          const {description,icon}=data.weather[0];
-
-          temperatureDegree.textContent= temp;
+          const { temp } = data.main;
+          const { description, icon } = data.weather[0];
+          //putting values from api to website
+          temperatureDegree.textContent = temp;
           temperatureDescription.textContent = description;
-          locationTimeZone.textContent=data.name;
-          document.querySelector(".icon").src= "https://openweathermap.org/img/wn/"+ icon + ".png";
+          locationTimeZone.textContent = data.name;
+          //setting icon
+          document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         })
     })
   }
